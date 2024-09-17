238d473a1ff96095743827cb59103f92ee575cf6
Rashad DuPaty <radupaty@gmail.com>
2024-09-17 11:03:06 +0900

    replaced header text

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,3 +1,25 @@
+gsap.from(".text", 0.8, {
+  y: 40,
+  opacity: 0,
+  ease: "power2.inOut",
+  delay: 1,
+});
+
+gsap.from(".loader", 2, {
+  width: 0,
+  ease: "power4.inOut",
+  delay: 2,
+});
+
+gsap.to(".pre-loader", 2, {
+  top: "-100%",
+  ease: "power4.inOut",
+  delay: 4,
+});
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
   let listTab = document.querySelectorAll('.tab');
